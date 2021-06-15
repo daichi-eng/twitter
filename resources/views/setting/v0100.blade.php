16d874f2dc9463a01d77a7445cdf76da7b82f2c8
daichi yamano <daichi.free.life@gmail.com>
2021-06-15 22:08:57 +0900

    v0100基本設定機能画面の更新

--- resources/views/setting/v0100.blade.php
+++ resources/views/setting/v0100.blade.php
@@ -17,34 +17,74 @@
 			</div>
 		</div>
 
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="card m-1">
-                    <div class="card-header">
-                        基本設定
-                    </div>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item"><a href="" target="_blank">予約投稿</a></li>
-                      <li class="list-group-item"><a href="" target="_blank">Dapibus ac facilisis in</a></li>
-                      <li class="list-group-item"><a href="" target="_blank">Vestibulum at eros</a></li>
-                    </ul>
-                  </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="card m-1">
-                    <div class="card-header">
-                        ツイート
-                    </div>
+		
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-header">Twitter情報<div>
+
+					<div class="card-body">
+						<form method="POST" action="{{ route('login') }}">
+							@csrf
+
+							<div class="form-group row">
+								<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+								<div class="col-md-6">
+									<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+									@error('email')
+										<span class="invalid-feedback" role="alert">
+											<strong>{{ $message }}</strong>
+										</span>
+									@enderror
+								</div>
+							</div>
+
+							<div class="form-group row">
+								<label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+								<div class="col-md-6">
+									<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="" required autocomplete="current-password">
+
+									@error('password')
+										<span class="invalid-feedback" role="alert">
+											<strong>{{ $message }}</strong>
+										</span>
+									@enderror
+								</div>
+							</div>
+
+							<div class="form-group row">
+								<div class="col-md-6 offset-md-4">
+									<div class="form-check">
+										<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+										<label class="form-check-label" for="remember">
+											{{ __('Remember Me') }}
+										</label>
+									</div>
+								</div>
+							</div>
+
+							<div class="form-group row mb-0">
+								<div class="col-md-8 offset-md-4">
+									<button type="submit" class="btn btn-primary">
+										{{ __('Login') }}
+									</button>
+
+									@if (Route::has('password.request'))
+										<a class="btn btn-link" href="{{ route('password.request') }}">
+											{{ __('Forgot Your Password?') }}
+										</a>
+									@endif
+								</div>
+							</div>
+						</form>
+					</div>
 				</div>
-            </div>
-            <div class="col-sm-4">
-                <div class="card m-1">
-                    <div class="card-header">
-                        メンション機能
-                    </div>
-				</div>	
-            </div>
-        </div>
+			</div>
+		</div>
     </div>
 
 </main>
